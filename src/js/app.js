c32dc7e4292e3716e54961d55e3c161d868671e5
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2017-12-03 23:11:33 +0300

    add styles,add libraries

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
-import sayHello from './lib/sayHello.js';
+import Barba from 'barba.js';
+import {TimelineMax} from 'gsap';
 
-sayHello();
+
+Barba.Dispatcher.on('linkClicked', function(el) {
+  // lastClicked = el;
+});
+
+var ExpandTransition = Barba.BaseTransition.extend({
+  start: function() {
+    Promise
+      .all([this.newContainerLoading, this.zoom()])
+      .then(this.showNewPage.bind(this));
+  },
+
+  zoom: function() {
+    var deferred = Barba.Utils.deferred();
+
+    let tl = new TimelineMax();
+    tl.to('body',1,{y:100,onComplete: function() {
+      deferred.resolve();
+    }});
+    return deferred.promise;
+  },
+
+  showNewPage: function() {
+    this.done();
+  }
+});
+
+Barba.Pjax.getTransition = function() {
+  var transitionObj = ExpandTransition;
+
+  //Barba.HistoryManager.prevStatus().namespace
+  return transitionObj;
+};
+
+
+Barba.Pjax.start();
